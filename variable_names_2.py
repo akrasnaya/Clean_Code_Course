43fc5d3d3863950d2fd561ad1a64c087cfd44d00
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-21 21:16:47 +0300

    Create variable_names_2.py

---
+++ variable_names_2.py
@@ -0,0 +1,14 @@
+"""
+result -> prediction
+sum -> sum_com (com - center of mass)
+bond_len -> bond_len_ang (ang - angstrem)
+count -> sum_of_nucleotide
+data -> dna_data
+sin -> tor_angle_sin
+cos -> tor_angle_cos
+matrix -> distance_matrix
+atom1 -> first_atom
+csv -> data_from_csv
+st -> sequence_string
+id -> chain_id
+"""
