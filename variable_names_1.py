2d13fb3e18d3b7c996057eba5aa538a6d5ef1cf3
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-21 20:32:44 +0300

    Create variable_names_1.py

---
+++ variable_names_1.py
@@ -0,0 +1,25 @@
+"""
+7.1.
+w -> is_win (win)
+l -> is_lose (lose)
+key -> is_key
+ion -> is_ion
+struct -> right_structure
+
+
+7.2. 
+empty
+full
+failed
+
+
+7.3. 
+i -> epoch
+
+7.4. 
+process_in
+process_off
+
+7.5.
+new_s -> new_sequence_list
+"""
