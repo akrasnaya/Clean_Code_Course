6a06af2d654354bc2e78216bb5a5361ef9ac6211
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-21 21:53:41 +0300

    Create class_names.py

---
+++ class_names.py
@@ -0,0 +1,16 @@
+"""
+3.1. 
+DetectWrong -> WrongSequencesDetector
+Data -> Dataset
+UserPlay -> UserPlayer
+MachinePlay -> MachinePlayer
+Processor -> FeaturesCreator
+
+3.2. 
+take_data -> get_data
+get_sequence -> get_sequence
+get_angles -> calculate_angles
+matrix -> calculate_matrix
+get_natrix -> get_natrix 
+take_mass -> calculate_mass
+"""
