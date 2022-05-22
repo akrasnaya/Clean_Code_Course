67c99b717b4d3c1489053b8fc2d6b5a581d90396
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-22 12:25:58 +0300

    Create interfaces.py

---
+++ interfaces.py
@@ -0,0 +1,8 @@
+"""
+transport_rna = RNA.create_rna_from_seq('AAGU')
+(Метод rna_from_seq создает структуру из стороковой последовательности и возвращает объект RNA, в то время как
+по умолчанию конструктор RNA создает структуру по индексу.)
+
+triangle_loss = Loss.set_type_of_loss('triangle')
+(Метод set_type_of_loss возвращает объект класса Loss с определенными полями)
+"""
