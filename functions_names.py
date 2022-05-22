f97c3f37c866a432344776d252ad13dcbd6c8635
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-22 12:02:18 +0300

    Create functions_names.py

---
+++ functions_names.py
@@ -0,0 +1,13 @@
+"""
+bond_angles -> calculate_bond_angles
+bond_distance -> calculate_bond_distance
+distance -> calculate_distance
+center_of_mass_chain -> calculate_center_of_mass_per_chain
+process_atom1 -> process_first_atom
+tors_angles -> calculate_tors_angles
+last_atom -> process_last_atom
+run -> run_preprocessing_data
+ocurrences -> is_occurance
+metrics -> calculate_metrics
+drop_ions -> filter_ions
+"""
