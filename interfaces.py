a73d865f2b00af68da65cfb94124601c314d3800
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-22 12:29:00 +0300

    Update interfaces.py

--- interfaces.py
+++ interfaces.py
@@ -5,4 +5,8 @@ transport_rna = RNA.create_rna_from_seq('AAGU')
 
 triangle_loss = Loss.set_type_of_loss('triangle')
 (Метод set_type_of_loss возвращает объект класса Loss с определенными полями)
+
+empty_structure = RNA.create_empty(5)
+(Метод empty_structure создает пустую структуру определенной длины и возвращает объект RNA, в то время как
+по умолчанию конструктор RNA создает структуру по индексу.)
 """
