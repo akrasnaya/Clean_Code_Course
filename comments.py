ffa96975ac863e6f8b4d6c18b3dc2b26a3bf23ea
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2022-05-30 20:05:32 +0300

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,22 @@
+"""
+def create_seq_pos(max_num=80):
+
+    #
+    # get the random length of the sequence
+    #
+
+def create_dummy_dataset(N_seq = 100, max_num = 80):
+
+    #
+    # create a trial dataset
+    #
+
+def run_data(data: Dataset):
+
+    # run preprocessing
+    
+ # validation step
+       with torch.set_grad_enabled(False):
+           loss_val_sum = 0.0
+           val_deviation_sum =0.0
+"""
